6a63ece8b5169553f426a82c6cffd9a45041fedc
ElBorry <borriello1985@gmail.com>
2024-11-02 18:33:30 -0300

    Descripción de los cambios realizados

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Mostrar el navbar cuando el usuario haga scroll hacia abajo
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() > 100) {  // Mostrar el navbar después de hacer scroll 100px
         $('#mainNav').fadeIn();  // Aparecer suavemente
     } else {
@@ -13,4 +13,28 @@ const navMenu = document.querySelector('nav ul');
 
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('hidden');
+});
+
+
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
 });
